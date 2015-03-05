c560d8091328db6ab8fedcc4f968b2f016c79305
Jaime Olmo <jamesxv7@gmail.com>
2015-03-05 15:52:44 -0400

    Update working-with-directories.ps1

--- working-with-directories.ps1
+++ working-with-directories.ps1
@@ -1,2 +1,6 @@
 #To get the content of a directory you can use
 $files = Get-ChildItem "C:\Users\Batmam\Documents\My Files\"
+
+for ($i=0; $i -lt $files.Count; $i++) {
+  $outfile = $files[$i].FullName 
+}
