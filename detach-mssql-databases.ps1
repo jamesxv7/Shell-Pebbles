82ac88f50e4956ff6ffe744c4b9ac7b84bd2a222
Jaime Olmo <jamesxv7@gmail.com>
2015-04-13 14:47:09 -0400

    Update detach-mssql-databases.ps1
    Comments added to clarify script execution.

--- detach-mssql-databases.ps1
+++ detach-mssql-databases.ps1
@@ -2,18 +2,22 @@ Set-ExecutionPolicy Unrestricted
 # 
 # Get SQL Server databases. 
 # 
+
+# Initiate $databases variable as an array list
 $databases = [System.Collections.ArrayList]@()
 
+# The Microsoft.SqlServer.Smo object is required for this script to work.
 [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | out-null 
-
 $svr = new-object Microsoft.SqlServer.Management.Smo.Server
 
+# Iterate through databases to generate a list matching only with DatabaseNames_ string. 
 foreach ($y in $svr.Databases ) {
     if ($y.Name.Contains("DatabaseNames_")) {
         $databases.Add($y.Name)
     } 
 }           
-      
+
+# Iterate through the list to detach only the items in the list. 
 foreach ($database in $databases) { 
     #$db = $database[$i].toString()
     
